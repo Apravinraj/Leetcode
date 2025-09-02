6fe1ad0e4e72f71e3ff034825dfe48a624ab6bf3
Pravin raj.A <118707879+Apravinraj@users.noreply.github.com>
2025-09-02 09:40:57 +0530

    Time: 12 ms (55.79%), Space: 42.4 MB (86.28%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean isIsomorphic(String s, String t) {
         if(s.length() != t.length()) return false; 
         HashMap<Character, Character> hm1 = new HashMap<>(); 
-        HashMap<Character, Character> hm2 = new HashMap<>(); 
 
         for(int i = 0 ; i < s.length() ; i++){
             if(hm1.containsKey(s.charAt(i))){
@@ -10,21 +9,24 @@ class Solution {
                     return false;
                 }
             }
+            else if (hm1.containsValue(t.charAt(i))) {
+                return false;
+            }
             else{
                 hm1.put(s.charAt(i),t.charAt(i));
             }
         }
 
-        for(int i = 0 ; i < t.length() ; i++){
-            if(hm2.containsKey(t.charAt(i))){
-                if(hm2.get(t.charAt(i))!=s.charAt(i)){
-                    return false;
-                }
-            }
-            else{
-                hm2.put(t.charAt(i),s.charAt(i));
-            }
-        }
+        // for(int i = 0 ; i < t.length() ; i++){
+        //     if(hm2.containsKey(t.charAt(i))){
+        //         if(hm2.get(t.charAt(i))!=s.charAt(i)){
+        //             return false;
+        //         }
+        //     }
+        //     else{
+        //         hm2.put(t.charAt(i),s.charAt(i));
+        //     }
+        // }
         return true;
     }
 }
